dbe621b4c2db9bb467a0de5495afcdf2cc1da2ad
Emmanuel Américo Barros <manuamericobarros@gmail.com>
2026-05-17 11:11:13 +0000

    Novato iniciando classe explorador

--- Explorador.java
+++ Explorador.java
@@ -3,27 +3,36 @@
  * Define a estrutura básica para diferentes tipos de exploradores.
  */
 public abstract class Explorador {
+    private String nome;
+    private String tipo;
+    private int prioridade;
+    private String tarefa;
     
-    // * Construtor que inicializa todos os atributos do explorador.
-    
-
+    // Construtor que inicializa todos os atributos do explorador.
+    public Explorador(String nome, String tipo, int prioridade, String tarefa) {
+        this.nome = nome;
+        this.tipo = tipo;
+        this.prioridade = prioridade;
+        this.tarefa = tarefa;
+    }
 
     /**
      * Método abstrato que deve ser implementado pelas subclasses.
      * Define como cada tipo de explorador executa sua tarefa.
      * @throws TarefaInvalidaException Se a tarefa for inválida
      */
-    public abstract void executarTarefa() throws TarefaInvalidaException;
+    public abstract void ejecutarTarefa() throws TarefaInvalidaException;
     
     /**
      * Exibe o status completo do explorador com formatação clara.
      */
     public void exibirStatus() {
-
+        System.out.println("Explorador: " + nome + " | Tipo: " + tipo + " | Status: " + tarefa);
     }
     
     // Getters para acesso aos atributos encapsulados
-
-
+    public String getNome() { return nome; }
+    public String getTipo() { return tipo; }
+    public int getPrioridade() { return prioridade; }
+    public String getTarefa() { return tarefa; }
 }
-
